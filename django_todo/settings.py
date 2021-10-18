0a89efdccaee9f52d2e5748649865fe9b48f6e5a
meldissa <melanie_9401@yahoo.co.uk>
2021-10-18 19:26:48 +0000

    Add secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&p_ru3hme$h)d&-7l$3oxzmbb1_r*=t_n^^z!7-8t3o16++awa')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
